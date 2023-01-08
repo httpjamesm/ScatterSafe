d312d80454f740c63be0a8268fab628a64347dba
httpjamesm <github@httpjames.space>
2023-01-07 22:14:20 -0500

    feat: change app theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,29 @@
-<script>
+<script lang="ts">
     import Recover from "$lib/Recover.svelte";
     import Split from "$lib/Split.svelte";
     import { Tabs, Tab, TabContent } from "carbon-components-svelte";
+    import { appWindow } from "@tauri-apps/api/window";
+    import { emit, listen } from "@tauri-apps/api/event";
+    import type { UnlistenFn } from "@tauri-apps/api/event";
+    import { onMount } from "svelte";
+
+    const checkTheme = async () => {
+        // check system theme and change html
+        const theme = await appWindow.theme();
+
+        document.documentElement.setAttribute(
+            "theme",
+            theme === "light" ? "light" : "g100"
+        );
+
+        await appWindow.onThemeChanged(() => {
+            checkTheme();
+        });
+    };
+
+    onMount(() => {
+        checkTheme();
+    });
 </script>
 
 <div class="parent">
